803451555c83d4b79001f7ad085d5ce22c93dda9
Ludovic Esperce <ludovic.esperce@afpa.fr>
2026-06-18 06:47:00 +0200

    Ajout possibilité de lire variable d'environnement dans Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'node',
-    globals: true,
-  },
-  base: import.meta.env.VITE_BASE_URL || '/',
+
+export default defineConfig(({ mode }) => {
+  // const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, '.')
+
+  return {
+    plugins: [react()],
+    test: {
+      environment: 'node',
+      globals: true,
+    },
+    base: env.VITE_BASE_URL || '/',
+  }
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     environment: 'node',
+//     globals: true,
+//   },
+//   base: env.VITE_BASE_URL || '/',
+// })
